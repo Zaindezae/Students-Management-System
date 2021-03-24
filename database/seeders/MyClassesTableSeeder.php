4ca466eeb47d964019bfc0aa5f6769420cd97fd2
Zain <abidelamkulath@gmail.com>
2021-03-24 23:48:00 +0530

    BCA

--- database/seeders/MyClassesTableSeeder.php
+++ database/seeders/MyClassesTableSeeder.php
@@ -19,24 +19,24 @@ class MyClassesTableSeeder extends Seeder
         $ct = ClassType::pluck('id')->all();
 
         $data = [
-            ['name' => '1st Sem', 'class_type_id' => $ct[0]],
-            ['name' => '2nd Sem', 'class_type_id' => $ct[0]],
-            ['name' => '3rd Sem', 'class_type_id' => $ct[0]],
-            ['name' => '4th Sem', 'class_type_id' => $ct[0]],
-            ['name' => '5th Sem', 'class_type_id' => $ct[0]],
-            ['name' => '6th Sem', 'class_type_id' => $ct[0]],
-            ['name' => '1st Sem', 'class_type_id' => $ct[1]],
-            ['name' => '2nd Sem', 'class_type_id' => $ct[1]],
-            ['name' => '3rd Sem', 'class_type_id' => $ct[1]],
-            ['name' => '4th Sem', 'class_type_id' => $ct[1]],
-            ['name' => '5th Sem', 'class_type_id' => $ct[1]],
-            ['name' => '6th Sem', 'class_type_id' => $ct[1]],
-            ['name' => '1st Sem', 'class_type_id' => $ct[2]],
-            ['name' => '2nd Sem', 'class_type_id' => $ct[2]],
-            ['name' => '3rd Sem', 'class_type_id' => $ct[2]],
-            ['name' => '4th Sem', 'class_type_id' => $ct[2]],
-            ['name' => '5th Sem', 'class_type_id' => $ct[2]],
-            ['name' => '6th Sem', 'class_type_id' => $ct[2]]
+            ['name' => 'BCA 1st Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 2nd Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 3rd Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 4th Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 5th Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 6th Sem', 'class_type_id' => $ct[0]],
+            ['name' => 'BBA 1st Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 2nd Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 3rd Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 4th Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 5th Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 6th Sem', 'class_type_id' => $ct[1]],
+            ['name' => 'BCom 1st Sem', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 2nd Sem', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 3rd Sem', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 4th Sem', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 5th Sem', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 6th Sem', 'class_type_id' => $ct[2]]
             ];
 
         DB::table('my_classes')->insert($data);
