d646c9d6ed3f1e78ddfd17ed55a1e9089b44caa2
Zain <abidelamkulath@gmail.com>
2021-03-25 15:26:58 +0530

    yrs

--- database/seeders/MyClassesTableSeeder.php
+++ database/seeders/MyClassesTableSeeder.php
@@ -19,24 +19,15 @@ class MyClassesTableSeeder extends Seeder
         $ct = ClassType::pluck('id')->all();
 
         $data = [
-            ['name' => 'BCA 1st Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BCA 2nd Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BCA 3rd Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BCA 4th Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BCA 5th Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BCA 6th Sem', 'class_type_id' => $ct[0]],
-            ['name' => 'BBA 1st Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BBA 2nd Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BBA 3rd Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BBA 4th Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BBA 5th Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BBA 6th Sem', 'class_type_id' => $ct[1]],
-            ['name' => 'BCom 1st Sem', 'class_type_id' => $ct[2]],
-            ['name' => 'BCom 2nd Sem', 'class_type_id' => $ct[2]],
-            ['name' => 'BCom 3rd Sem', 'class_type_id' => $ct[2]],
-            ['name' => 'BCom 4th Sem', 'class_type_id' => $ct[2]],
-            ['name' => 'BCom 5th Sem', 'class_type_id' => $ct[2]],
-            ['name' => 'BCom 6th Sem', 'class_type_id' => $ct[2]]
+            ['name' => 'BCA 1st Yr', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 2nd Yr', 'class_type_id' => $ct[0]],
+            ['name' => 'BCA 3rd Yr', 'class_type_id' => $ct[0]],
+            ['name' => 'BBA 1st Yr', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 2nd Yr', 'class_type_id' => $ct[1]],
+            ['name' => 'BBA 3rd Yr', 'class_type_id' => $ct[1]],
+            ['name' => 'BCom 1st Yr', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 2nd Yr', 'class_type_id' => $ct[2]],
+            ['name' => 'BCom 3rd Yr', 'class_type_id' => $ct[2]],
             ];
 
         DB::table('my_classes')->insert($data);
